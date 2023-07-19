c13730bd62b8698183788a7f6c59d5d965c6374f
jones <selvajones150@gmail.com>
2023-07-19 10:55:09 +0530

    pattern1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var n=parseInt(prompt("Enthr The Value"));
+
+
+for(let i=1;i<=n;i++)
+{
+for (let j=1;j<=i; j++)
+    {
+		document.write("*");
+		document.write("&nbsp");
+	}
+     document.write("<br>");	
+}
